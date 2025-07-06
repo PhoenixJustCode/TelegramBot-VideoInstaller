5acfa0f35188906e5397ca923b9a59e5194d895e
Alexander Demchenko <alexandersd3306@gmail.com>
2025-07-06 20:14:25 +0500

    new rule for correct url links

--- internal/utils/string_utils.go
+++ internal/utils/string_utils.go
@@ -3,10 +3,12 @@ package utils
 import "strings"
 
 func IsYoutubeURL(url string) bool {
-    return strings.Contains(url, "youtube") || strings.Contains(url, "youtu.be")
+	return (strings.Contains(url, "youtube.com/watch?v=") || strings.Contains(url, "youtu.be/")) &&
+		!strings.Contains(url, "search_query") &&
+		!strings.Contains(url, "/channel/") &&
+		!strings.Contains(url, "/user/")
 }
 
-
 func IsMP4File(url string) bool {
-    return strings.HasSuffix(url, ".mp4") 
+	return strings.HasSuffix(url, ".mp4")
 }
