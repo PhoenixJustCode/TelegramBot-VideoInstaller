4a3366cc66751592173234ee1725b22b252e3a42
Alexander Demchenko <alexandersd3306@gmail.com>
2025-07-07 23:05:47 +0500

    MkdirAll add to init func for audio/video files

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,7 @@ import (
 	"os"
 	"telegramBotInstaller/internal/bot"
 	"telegramBotInstaller/internal/config"
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -16,12 +16,15 @@ func init() {
 	})
 
 	log.SetOutput(os.Stdout)
-
 	log.SetLevel(log.DebugLevel)
 
-	if err := godotenv.Load(); err != nil {
-		log.Warnf("❗ Не удалось загрузить .env файл: %v", err)
-	}
+	os.MkdirAll("tmp/video", 0755)
+	os.MkdirAll("tmp/audio", 0755)
+
+
+	// if err := godotenv.Load(".env"); err != nil {
+	// 	log.Warnf("❗ Не удалось загрузить .env файл: %v", err)
+	// }
 }
 
 
